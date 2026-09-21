c95659bc8fbede844fad28b9a4a733d5d126c930
guillaumegirishya <guillaumegirishya@gmail.com>
2026-09-21 17:02:12 -0500

    doc: update on the firebase-config name

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase connection details for the House Damage Log (project: roommates-damage-log).
+// This must sit in the same folder as index.html when deployed.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDfbB2gcLFo2am0uaxFibhNsyFcerDjgzs",
+  authDomain: "roommates-damage-log.firebaseapp.com",
+  projectId: "roommates-damage-log",
+  storageBucket: "roommates-damage-log.firebasestorage.app",
+  messagingSenderId: "1010520091633",
+  appId: "1:1010520091633:web:67aa67b0299e534c527250"
+};
